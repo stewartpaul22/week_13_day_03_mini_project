7ddb3de284f2a0268594c58eeec48e0904e44457
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-18 17:35:38 +0100

    Added app.js request to get all countries from the restcountry api

--- client/src/app.js
+++ client/src/app.js
@@ -1,1 +1,20 @@
 // Initial app.js
+const Request = require('./services/requests.js');
+
+const requestBucketList = new Request('http://localhost:3000/api/bucket_list');
+const requestRestCountries = new Request('https://restcountries.eu/rest/v2/all');
+
+const getRestCountriesComplete = function(allCountries){
+  for(let country of allCountries){
+    console.log("Country Name: ", country.name);
+  }
+}
+
+
+const appStart = function(){
+  console.log("DOM content loaded, app starting...");
+  requestRestCountries.get(getRestCountriesComplete);
+}
+
+
+document.addEventListener('DOMContentLoaded', appStart);
