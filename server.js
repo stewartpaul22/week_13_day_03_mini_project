e21c51e59f104d5b3ce281e51cb434e0eed17187
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-04-18 14:40:46 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("client/build"));
+
+const MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect("mongodb://localhost:27017", function(err, client){
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const db = client.db("bucket_list_db");
+
+  console.log("Connected to db");
+})
